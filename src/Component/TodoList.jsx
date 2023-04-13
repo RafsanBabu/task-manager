6ef3db3c8244ec6c88aed94aec2b466b3f6de596
RafsanBabu <rafsan.cse.ru13@gmail.com>
2023-04-14 04:01:04 +0600

    drag and drop issue implemented

--- src/Component/TodoList.jsx
+++ src/Component/TodoList.jsx
@@ -8,7 +8,10 @@ import '../App.css'
 import handleTaskReducer from '../Redux/Reducer/handleTaskReducer';
 function TodoList() {
   const tasks = useSelector((taskState) => taskState);
-
+  const handleDragSDtop = (e, data) => {
+    localStorage.setItem('taskPosition', { valueX: data.x, valueY: data.y });
+    console.log(data.x, data.y);
+  };
   return (
     <div className='row'>
         <div className='todo-list  d-flex justify-content-around'>
@@ -19,7 +22,7 @@ function TodoList() {
               {
                 tasks.map((item) => {
                   return(
-                  <Draggable>
+                  <Draggable defaultPosition={{ x: 0, y: 0 }} onStop={(e,data)=>handleDragSDtop(e,data)}>
                     <div className='list-item text-center'>
                       <p className='list-item text-center'>{item.newTask}</p>
                     </div>
@@ -28,13 +31,13 @@ function TodoList() {
               }
               
           </div>
-          <div className='col-md-3 text-center todo-list-column'>
+          <div className='col-md-3 text-center todo-list-column inProgress-area'>
               
-              <label className='text-center list-item-header'>
+              <label className='text-center list-item-header '>
               In progress
               </label>
           </div>
-          <div className='col-md-3 text-center todo-list-column'>
+          <div className='col-md-3 text-center todo-list-column done-area'>
             <label className='text-center list-item-header'>Done</label>
           </div>
         </div>
